16f03a1354bddef53280d6b8a29650e50a8ea869
RishikaReddy <rishikareddy2710@gmail.com>
2023-12-06 23:39:47 +0530

    First commit

---
+++ p1.py
@@ -0,0 +1,11 @@
+
+
+
+age=int(input("age:"))
+                                                                   
+
+if (age>18):
+    print("Eligible to vote")
+
+else:
+    print("Not eligible to vote")
